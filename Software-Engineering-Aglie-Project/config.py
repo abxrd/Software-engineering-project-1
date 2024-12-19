e9d96438e6b489519c7dacbd98a8e0ae67c92d46
abxrd <70892779+abxrd@users.noreply.github.com>
2024-12-19 22:57:44 +0000

    Add files via upload

--- Software-Engineering-Aglie-Project/config.py
+++ Software-Engineering-Aglie-Project/config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI= ('postgresql://neondb_owner:aAqfh58BHuLG@ep-restless-glitter-a2ab5pov-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require')
+    SECRET_KEY = os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = ('postgresql://neondb_owner:aAqfh58BHuLG@ep-restless-glitter-a2ab5pov-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///Software-Engineering-Aglie-Project.db'
-
     SQLALCHEMY_TRACK_MODIFICATIONS = False
